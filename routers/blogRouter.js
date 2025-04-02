b6c2e5d9a9e31fb6d43b9625f7638979283966c4
Bhargavbhimani229 <bhargavbhimani229@gmail.com>
2025-04-02 13:31:48 +0530

    done

--- routers/blogRouter.js
+++ routers/blogRouter.js
@@ -4,7 +4,7 @@ const blogRoter = Router();
 
 const blogController = require("../controllers/blogController");
 const upload = require("../middlewares/blogMiddleware");
-const userCred = require("../models/pwSchema");
+
 
 blogRoter.get("/",blogController.homePage);
 
@@ -22,24 +22,5 @@ blogRoter.get("/register",blogController.registerPage);
 blogRoter.get("/login" , blogController.loginPage);
 blogRoter.post("/createCred", blogController.createCred);
 
-blogRoter.post('/login', async (req, res) => {
-  try {
-  const { username, password } = req.body;
-  const user = await userCred.findOne({ username });
-  if (!user) {
-  return res.status(401).json({ error: 'Authentication failed' });
-  }
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if (!passwordMatch) {
-  return res.status(401).json({ error: 'Authentication failed' });
-  }
-  const token = jwt.sign({ userId: user._id }, 'your-secret-key', {
-  expiresIn: '1h',
-  });
-  res.status(200).json({ token });
-  } catch (error) {
-  res.status(500).json({ error: 'Login failed' });
-  }
-  })
 
 module.exports = blogRoter; 
